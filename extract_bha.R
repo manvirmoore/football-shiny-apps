2b8679bf9545cb55d46d8d7b0a81b0fdeb7e7b23
Manni <manvir.moore@yahoo.co.uk>
2025-04-05 13:48:07 +0100

    explored the package a bit more

--- extract_bha.R
+++ extract_bha.R
@@ -1,9 +1,26 @@
-library("worldfootballR")
+#' Code to prep a shiny app. this code should get the stats of each current 
+#' Brighton player, to be selected via a shiny slicer.
 
+#import libraries
+library(worldfootballR)
+library(tidyverse)
+
+#get the right year for the season- if the second half of year, use the next.
+end_year <- if (as.numeric(format(Sys.Date(),"%m")) > 7) {
+  as.numeric(format(Sys.Date(),"%Y")) + 1
+} else {
+  as.numeric(format(Sys.Date(),"%Y"))
+}
+
+#get the urls
 current_prem <- fb_league_urls(country = "ENG", gender = "M", season_end_year = 2025) #make this dynamic
+all_team_urls <- c(strsplit(fb_teams_urls(current_prem),"/n"))
+bha_url <- all_team_urls[grepl("Brighton",all_team_urls, fixed = TRUE)]
+bha_url <- bha_url[[1]]
 
-bha <- fb_teams_urls("https://fbref.com/en/comps/9/Premier-League-Stats")[7] #how to find the right index
+#create a table of players
+bha_players <- strsplit(fb_player_urls(bha_url), "/n")
 
-bha_players <- fb_player_urls(bha)
-
-bha_player_stats <- fb_player_season_stats(bha_players[1], stat_type = "standard", national = FALSE, time_pause = 3) # make this a loop
+player1_stats <- fb_player_season_stats(bha_players[[1]], stat_type = "standard", national = FALSE, time_pause = 3) # make this a loop
+# this give a list of past seasons and teams too. need to convert all of this into tibbles i think. we want them all appended with the player name on the left. 
+#add also the record against each team hone and awat this season
